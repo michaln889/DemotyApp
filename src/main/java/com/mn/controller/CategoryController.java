d718842e588ef604a7c365ecfa81c745030534f7
Michal <michaln889@gmail.com>
2017-04-16 16:14:40 +0200

    find gifs by category id added

--- src/main/java/com/mn/controller/CategoryController.java
+++ src/main/java/com/mn/controller/CategoryController.java
@@ -1,10 +1,12 @@
 package com.mn.controller;
 
 import com.mn.data.CategoryRepository;
+import com.mn.data.GifRepository;
 import com.mn.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,6 +17,9 @@ public class CategoryController
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/categories")
     public String listCategories(ModelMap modelMap)
     {
@@ -22,4 +27,13 @@ public class CategoryController
         modelMap.put("categories", categories);
         return "categories";
     }
+
+    @RequestMapping("/category/{id}")
+    public String getCategory(@PathVariable int id, ModelMap modelMap)
+    {
+        modelMap.put("gifs", gifRepository.findByCategoryId(id));
+        modelMap.put("category", categoryRepository.findById(id));
+        return "category";
+    }
+
 }
